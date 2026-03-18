b097e57c3bcb01fe44e5470b8465513830e803c2
Artem <high.born.honey@gmail.com>
2026-03-18 16:14:11 +0300

    work on views

--- views/author/report.php
+++ views/author/report.php
@@ -7,29 +7,34 @@ use yii\helpers\Html;
 /* @var $year int */
 
 $this->title = "ТОП 10 авторов за {$year}";
+$this->params['breadcrumbs'][] = 'Отчет';
 ?>
-    <h1><?= Html::encode($this->title) ?></h1>
 
-<?php if (!empty($topAuthors)): ?>
-    <table class="table table-bordered">
-        <thead>
-        <tr>
-            <th>№</th>
-            <th>Автор</th>
-            <th>Книг</th>
-        </tr>
-        </thead>
-        <tbody>
-        <?php foreach ($topAuthors as $i => $author): ?>
-            <tr>
-                <td><?= $i + 1 ?></td>
-                <td><?= Html::encode($author['name']) ?></td>
-                <td><?= $author['book_count'] ?></td>
-            </tr>
-        <?php endforeach; ?>
-        </tbody>
-    </table>
-<?php else: ?>
-    <p>Книг за <?= $year ?> год не найдено.</p>
-<?php endif; ?>
+<div class="card">
+    <h1 class="card-header"><?= Html::encode($this->title) ?></h1>
+    <div class="card-body">
+        <?php if (!empty($topAuthors)): ?>
+            <table class="table table-bordered table-success table-striped table-hover">
+                <thead>
+                <tr>
+                    <th>№</th>
+                    <th>Автор</th>
+                    <th>Книг</th>
+                </tr>
+                </thead>
+                <tbody>
+                <?php foreach ($topAuthors as $i => $author): ?>
+                    <tr>
+                        <td><?= $i + 1 ?></td>
+                        <td><?= Html::encode($author['name']) ?></td>
+                        <td><span class="badge rounded-pill bg-primary"><?= $author['book_count'] ?></span></td>
+                    </tr>
+                <?php endforeach; ?>
+                </tbody>
+            </table>
+        <?php else: ?>
+            <p>Книг за <?= $year ?> год не найдено.</p>
+        <?php endif; ?>
 
+    </div>
+</div>
--- views/author/update.php
+++ views/author/update.php
@@ -6,9 +6,9 @@ use yii\helpers\Html;
 /** @var app\models\Author $model */
 
 $this->title = 'Обновить автора: ' . $model->name;
-$this->params['breadcrumbs'][] = ['label' => 'Authors', 'url' => ['index']];
+$this->params['breadcrumbs'][] = ['label' => 'Авторы', 'url' => ['index']];
 $this->params['breadcrumbs'][] = ['label' => $model->name, 'url' => ['view', 'id' => $model->id]];
-$this->params['breadcrumbs'][] = 'Update';
+$this->params['breadcrumbs'][] = 'Обновить';
 ?>
 <div class="author-update">
 
