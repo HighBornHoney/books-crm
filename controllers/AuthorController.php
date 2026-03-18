62a9d2361536f58dcac399fc5f78eeec4dae97d3
Artem <high.born.honey@gmail.com>
2026-03-18 17:25:38 +0300

    refactor report feature

--- controllers/AuthorController.php
+++ controllers/AuthorController.php
@@ -4,7 +4,6 @@ namespace app\controllers;
 
 use app\models\Author;
 use app\models\AuthorSearch;
-use Yii;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\AccessControl;
@@ -99,23 +98,8 @@ class AuthorController extends Controller
     {
         $year = $year ?: date('Y');
 
-        $sql = <<<SQL
-            SELECT a.id, a.name, COUNT(ba.book_id) AS book_count
-            FROM authors a
-            JOIN book_author ba ON ba.author_id = a.id
-            JOIN books b ON b.id = ba.book_id
-            WHERE b.year = :year
-            GROUP BY a.id, a.name
-            ORDER BY book_count DESC
-            LIMIT 10
-        SQL;
-
-        $topAuthors = Yii::$app->db->createCommand($sql)
-            ->bindValue(':year', $year)
-            ->queryAll();
-
         return $this->render('report', [
-            'topAuthors' => $topAuthors,
+            'topAuthors' => Author::getTopAuthorsByYear($year),
             'year' => $year,
         ]);
     }
--- models/Author.php
+++ models/Author.php
@@ -2,6 +2,7 @@
 
 namespace app\models;
 
+use Yii;
 use yii\db\ActiveQuery;
 use yii\db\ActiveRecord;
 
@@ -29,5 +30,27 @@ class Author extends ActiveRecord
     public function getSubscriptions(): ActiveQuery
     {
         return $this->hasMany(Subscription::class, ['author_id' => 'id']);
+    }
+
+    public static function getTopAuthorsByYear(int $year): array
+    {
+        $sql = <<<SQL
+            SELECT
+                a.id,
+                a.name,
+                COUNT(ba.book_id) AS book_count
+            FROM authors a
+            JOIN book_author ba ON ba.author_id = a.id
+            JOIN books b ON b.id = ba.book_id
+            WHERE b.year = :year
+            GROUP BY a.id, a.name
+            ORDER BY book_count DESC
+            LIMIT 10
+        SQL;
+
+        return Yii::$app->db
+            ->createCommand($sql)
+            ->bindValue(':year', $year)
+            ->queryAll();
     }
 }
